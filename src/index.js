32b2b92f3e5094cc779cd10844bf87146384cdd6
Tal Feldman <tal.feldman.1991@gmail.com>
2023-12-14 16:32:07 +0200

    add server

--- src/index.js
+++ src/index.js
@@ -323,7 +323,7 @@ io.on("connection", (socket) => {
     })
 })
 
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3001, () => {
 
     
     console.log("Server is running on port 3001");
